262284563fff79dc4ecebc729226751f8578eb2a
Joe Maples <joe@frap129.org>
2017-05-12 21:08:01 -0500

    cpufreq: Add Electron governor
    Electron is a governor based on interactive from the latest MSM8994 CAF branch with more upstream improvements, powersave bias,
    screen off max frequency, and some other tweaks to improve battery life without hindering performance.
    
    Thanks to Google, CAF, Linaro, imoseyon, neobuddy, and many others.
    
    Signed-off-by: Joe Maples <joe@frap129.org>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -440,6 +440,7 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
+<<<<<<< HEAD
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELEMENTALX)
 extern struct cpufreq_governor cpufreq_gov_elementalx;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_elementalx)
@@ -461,6 +462,11 @@ extern struct cpufreq_governor cpufreq_gov_smartmax_eps;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
 extern struct cpufreq_governor cpufreq_gov_pegasusq;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_pegasusq)
+=======
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ELECTRON)
+extern struct cpufreq_governor cpufreq_gov_electron;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_electron)
+>>>>>>> 8ab64ef... cpufreq: Add Electron governor
 #endif
 
 /*********************************************************************
